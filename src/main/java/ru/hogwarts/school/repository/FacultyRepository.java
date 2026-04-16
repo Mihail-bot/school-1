a56b44096f6522446970b80eb6a22242bec91589
Miha <shmihail85@yandex.ru>
2026-04-16 22:44:54 +0300

    three commit

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -3,6 +3,6 @@ package ru.hogwarts.school.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Faculty;
 
-public interface FacultyRepository extends JpaRepository<Faculty, Long> {           
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
 
 }
